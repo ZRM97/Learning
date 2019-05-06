b020cfd0a55d5ab5fa1fcab4f0e4128bf52b580c
ZRM97 <zrmrrr@gmail.com>
2019-05-06 20:12:47 +0500

    Input

--- First.py
+++ First.py
@@ -1,4 +1,10 @@
 price = 50
+apple = 4.2
 print('Hello World!')
 print(price)
 print(price * 2)
+print(price * apple)
+
+name = input('What is your name? ')
+favourite_color = input('What is your favourite color? ')
+print(name + ' likes ' + favourite_color)
