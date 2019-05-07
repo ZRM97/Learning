3a5097b630607944cd6b1e403677e09534aa50de
ZRM97 <zrmrrr@gmail.com>
2019-05-07 18:15:36 +0500

    Changing Strings to variables

--- First.py
+++ First.py
@@ -8,3 +8,7 @@ print(price * apple)
 name = input('What is your name? ')
 favourite_color = input('What is your favourite color? ')
 print(name + ' likes ' + favourite_color)
+
+birth_year = input('Enter your birth year: ')
+age = 2019 - int(birth_year)
+print(age)
